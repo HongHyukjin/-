af4979a0b1e1427617e1c167fa8e8dc83f63955d
HongHyukJin <gurwlszx@naver.com>
2023-02-17 18:40:41 +0900

    0217

---
+++ 평가산출물/홍혁진/js/lib/kurly.js
@@ -0,0 +1,108 @@
+(function($){
+    const kurly={
+        init(){
+            this.header();
+            this.section1();
+        },
+        header(){
+            console.log('header');
+        },
+        section1(){
+
+            let cnt = 0;
+            let setId = 0;
+            //1.메인슬라이드
+            function mainSlide(){
+                console.log(cnt);
+                $('.slide-wrap').animate({left: `${-100 * cnt}%`}, 1000, 'easeInOutExpo', function(){
+                    if(cnt === 9) cnt = 0;
+                    $('.slide-wrap').animate({left: `${-100 * cnt}%`}, 0);
+                })
+            }
+            //2-1.다음카운트
+            function nextCount(){
+                cnt ++;
+                mainSlide();
+            }
+            //2-2.이전카운트
+            function prevCount(){
+                cnt --;
+                mainSlide();
+            }
+            //3.자동타이머
+            function autoTimer(){
+                setId = setInterval(nextCount, 3000)
+            }
+            autoTimer();
+
+            //이전버튼, 다음버튼
+            $('.next-btn').on({
+                mouseenter(){
+                    clearInterval(setId);
+                },
+                mouseleave(){
+                    autoTimer();
+                },
+                click(){
+                    if(!$('.slide-wrap').is(':animated')){
+                        nextCount();
+                    }
+                }
+            });
+            $('.prev-btn').on({
+                mouseenter(){
+                    clearInterval(setId);
+                },
+                mouseleave(){
+                    autoTimer();
+                },
+                click(){
+                    if(!$('.slide-wrap').is(':animated')){
+                        prevCount();
+                    }
+                }
+            });
+
+            //터치스와이프
+            let touchStart = 0;
+            let touchEnd = 0;
+            let dragStart = 0;
+            let dragEnd = 0;
+            let winWidth = $(window).innerWidth();
+            let mousedown = false;
+
+            $('#section1').on({
+                mousedown(e){
+                    mousedown = true;
+                    touchStart = e.clientX;
+                    dragStart = touchStart - $('.slide-wrap').offset().left;
+                    clearInterval(setId);
+                },
+                mouseup(e){
+                    mousedown = false;
+                    touchEnd = e.clientX;
+                    if(touchStart - touchEnd > 0){
+                        if(!$('.slide-wrap').is(':animated')){
+                            nextCount();
+                        }
+                    }
+                    if(touchStart - touchEnd < 0){
+                        if(!$('.slide-wrap').is(':animated')){
+                            prevCount();
+                        }
+                    }
+                    autoTimer();
+                },
+                mousemove(e){
+                    if(mousedown === false){
+                        return;
+                    }
+                    dragEnd = e.clientX;
+                    $('.slide-wrap').css({left : dragEnd - dragStart});
+                }
+            })
+            
+        }
+    }
+    kurly.init();
+})(jQuery);
