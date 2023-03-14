c07de2775be328d19df8cae5ebe535a65cfafb6c
HongHyukJin <gurwlszx@naver.com>
2023-03-15 01:18:54 +0900

    0315

---
+++ 포트폴리오/2차 포트폴리오/LotteHotel/lottehotel/src/components/WrapComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TopModalComponent from './wrap/TopModalComponent';
+
+export default function WrapComponent () {
+    return (
+        <div id='wrap'>
+            <TopModalComponent />     
+        </div>
+    );
+};
+
---
+++ 포트폴리오/2차 포트폴리오/LotteHotel/lottehotel/src/components/wrap/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function HeaderComponent (){
+    return (
+        <header id="header">
+            <div className="container">
+                <div className="gap">
+                    <div className="content">
+                        <div className="nav-top"></div>
+                        <div className="nav-bottom"></div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
