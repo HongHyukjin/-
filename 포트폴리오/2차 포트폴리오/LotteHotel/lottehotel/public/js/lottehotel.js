b9c7db07a2640d2c9ecd757072accfc84036198c
HongHyukJin <gurwlszx@naver.com>
2023-03-21 18:35:45 +0900

    0321

---
+++ 포트폴리오/2차 포트폴리오/LotteHotel/lottehotel/public/js/lottehotel.js
@@ -0,0 +1,13 @@
+import jQuery from "jquery";
+
+(function($, window){
+  const lottehotel = {
+    init(){
+      this.section1();
+    },
+    section1(){
+
+    }
+  }
+  lottehotel.init();
+})(jQuery, window)
--- 포트폴리오/2차 포트폴리오/LotteHotel/lottehotel/src/components/wrap/main/Section4Component.jsx
+++ 포트폴리오/2차 포트폴리오/LotteHotel/lottehotel/src/components/wrap/main/Section4Component.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default function Section4Component (){
+
+    React.useEffect(() => {
+        let cnt = 0;
+        let setId = 0;
+
+
+    })
+
     return (
         <section id="section4">
             <div className="container">
@@ -8,6 +17,9 @@ export default function Section4Component (){
                     <div className="slide-container">
                         <div className="slide-view">
                             <ul className="slide-wrap">
+                                <li className="slide slide1"></li>
+                                <li className="slide slide2"></li>
+                                <li className="slide slide3"></li>
                                 <li className="slide slide1"></li>
                             </ul>
                         </div>
