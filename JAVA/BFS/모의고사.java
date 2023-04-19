701688b713cff75343087de4c08a561b03a9c58c
HongHyukJin <gurwlszx@naver.com>
2023-04-19 20:19:48 +0900

    0419

--- JAVA/BFS/모의고사.java
+++ JAVA/BFS/모의고사.java
@@ -2,21 +2,35 @@ package BFS;
 import java.util.*;
 public class 모의고사 {
   public static int[] solution(int[] answers) {
-    int[] answer = new int[3];
-    int[][] choices = {
-      {1,2,3,4,5},
-      {2,1,2,3,2,4,2,5},
-      {3,3,1,1,2,2,4,4,5,5}
-    };
+    int[] answer = {};
+    int[] score = new int[3];
+    int[] first = {1,2,3,4,5};
+    int[] second = {2,1,2,3,2,4,2,5};
+    int[] third = {3,3,1,1,2,2,4,4,5,5};
 
     for(int i=0; i<answers.length; i++){
-      int cnt = 0;
-      for(int j=0; j<3; j++){
-        if(answers[i] == choices[j][i]){
-          cnt++;
-          answer[j] ++;
-        }
+
+      if(answers[i] == first[i%5]){
+        score[0]++;
       }
+      if(answers[i] == second[i%8]){
+        score[1]++;
+      }
+      if(answers[i] == third[i%10]){
+        score[2]++;
+      }
+    }
+    // 세명의 점수의 최대값
+    int max = Math.max(score[0], Math.max(score[1], score[2]));
+    ArrayList<Integer> list = new ArrayList<>();
+    for(int i=0; i<score.length; i++){
+      if(score[i] == max){
+        list.add(i+1);
+      }
+    }
+    answer = new int[list.size()];
+    for(int i=0; i<list.size(); i++){
+      answer[i] = list.get(i);
     }
 
     return answer;
