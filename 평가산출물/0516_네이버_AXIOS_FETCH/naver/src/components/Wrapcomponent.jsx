66f6b922b66112c3787ac11a70590063877a1e5a
HongHyukJin <gurwlszx@naver.com>
2023-05-18 18:02:34 +0900

    0518

---
+++ 평가산출물/0516_네이버_AXIOS_FETCH/naver/src/components/Wrapcomponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignupComponent from './wrap/SignupComponent';
+
+export default function Wrapcomponent (){
+    return (
+        <div id="wrap">
+            <SignupComponent />
+        </div>
+    );
+};
+
