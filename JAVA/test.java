f2c0abc3c3970bb52126c1d11b0685dd9c9a75a2
HongHyukJin <gurwlszx@naver.com>
2023-07-03 11:01:15 +0900

    0703

--- JAVA/test.java
+++ JAVA/test.java
@@ -2,9 +2,21 @@ import java.util.*;
 public class test {
 
   public static void main(String[] args) {
-    String num = "123";
-    System.out.println(num.substring(1,1));
+    List<Integer> list = new ArrayList<>();
 
+    list.add(3);
+    list.add(5);
+    list.add(7);
+
+    int cnt = 0;
+    System.out.println(list.size());
+    for(Integer n : list){
+      cnt++;
+      if(cnt == list.size()){
+        System.out.println("끝");
+      }
+      System.out.println(n);
+    }
     
   }
 }
